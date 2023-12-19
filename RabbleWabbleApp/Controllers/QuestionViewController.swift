6b8a90b7893827529f7597b84f885a83e27be3f5
renato <felicio95.jr@gmail.com>
2023-12-19 09:34:42 -0300

    chore: folders organization and fixes linter warnings

---
+++ RabbleWabbleApp/Controllers/QuestionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  RabbleWabbleApp
+//
+//  Created by Renato F. dos Santos Jr on 19/12/23.
+//
+
+import UIKit
+
+public class QuestionViewController: UIViewController {
+
+}
