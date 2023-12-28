7bea134874198679a7dffa1f94f9251efa400bb5
renato <felicio95.jr@gmail.com>
2023-12-28 16:40:11 -0300

    feat: adds logic for presenting questions and count the correct and incorrect ones

--- RabbleWabbleApp/Controllers/QuestionViewController.swift
+++ RabbleWabbleApp/Controllers/QuestionViewController.swift
@@ -8,5 +8,60 @@
 import UIKit
 
 public class QuestionViewController: UIViewController {
+    // MARK: - Instance properties
+    public var questionGroup = QuestionGroup.basicPhrases()
+    public var questionIndex = 0
 
+    public var correctCount = 0
+    public var incorrectCount = 0
+
+    // swiftlint:disable force_cast
+    public var questionView: QuestionView! {
+        guard isViewLoaded else { return nil }
+        return (view as! QuestionView)
+    }
+    // swiftlint:enable force_cast
+
+    // MARK: - View LifeCycle
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        showQuestion()
+    }
+
+    private func showQuestion() {
+        let question = questionGroup.questions[questionIndex]
+
+        questionView.answerLabel.text = question.answer
+        questionView.promptLabel.text = question.prompt
+        questionView.hintLabel.text = question.hint
+
+        questionView.answerLabel.isHidden = true
+        questionView.hintLabel.isHidden = true
+    }
+
+    // MARK: - Actions
+    @IBAction func toggleAnswerLabels(_ sender: Any) {
+        questionView.answerLabel.isHidden = !questionView.answerLabel.isHidden
+        questionView.hintLabel.isHidden = !questionView.hintLabel.isHidden
+    }
+
+    @IBAction func handleCorrect(_ sender: Any) {
+        correctCount += 1
+        questionView.correctCountLabel.text = "\(correctCount)"
+        showNextQuestion()
+    }
+
+    @IBAction func handleIncorrect(_ sender: Any) {
+        incorrectCount += 1
+        questionView.incorrectCountLabel.text = "\(incorrectCount)"
+        showNextQuestion()
+    }
+
+    private func showNextQuestion() {
+        questionIndex += 1
+
+        guard questionIndex < questionGroup.questions.count else { return }
+
+        showQuestion()
+    }
 }
