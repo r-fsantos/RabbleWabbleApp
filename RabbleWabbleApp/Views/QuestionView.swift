f5810bc6a14620c17306b96e8a42004c48a4065a
renato <felicio95.jr@gmail.com>
2023-12-28 15:44:40 -0300

    feat: creates queestion view

---
+++ RabbleWabbleApp/Views/QuestionView.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionView.swift
+//  RabbleWabbleApp
+//
+//  Created by Renato F. dos Santos Jr on 28/12/23.
+//
+
+import UIKit
+
+public class QuestionView: UIView {
+    @IBOutlet public var answerLabel: UILabel!
+    @IBOutlet public var correctCountLabel: UILabel!
+    @IBOutlet public var incorrectCountLabel: UILabel!
+    @IBOutlet public var promptLabel: UILabel!
+    @IBOutlet public var hintLabel: UILabel!
+}
