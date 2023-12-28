480f93446af38aceaae4aba00942d293996a6ed8
renato <felicio95.jr@gmail.com>
2023-12-28 15:11:51 -0300

    chore: adds new assets and launch screen storyboard

---
+++ RabbleWabbleApp/Models/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  RabbleWabbleApp
+//
+//  Created by Renato F. dos Santos Jr on 28/12/23.
+//
+
+import Foundation
+
+public struct Question {
+    public let answer: String
+    public let hint: String?
+    public let prompt: String
+}
---
+++ RabbleWabbleApp/Models/QuestionGroup.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionGroup.swift
+//  RabbleWabbleApp
+//
+//  Created by Renato F. dos Santos Jr on 28/12/23.
+//
+
+import Foundation
+
+public struct QuestionGroup {
+    public let questions: [Question]
+    public let title: String
+}
